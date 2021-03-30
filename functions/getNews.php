41529076c35b26ef640d61b0a212f692f6a0f012
Kelver <kelver_kmv@hotmail.com>
2021-03-30 12:41:33 -0300

    att news

--- functions/getNews.php
+++ functions/getNews.php
@@ -6,10 +6,9 @@ function getNews($sigla)
     if(!$sigla || $sigla == ''){
         $sigla = 'Bitcoin';
     }
-    $domains = 'sapo.pt, publico.pt';
+    $domains = 'sapo.pt,publico.pt';
 
     $data = file_get_contents("http://newsapi.org/v2/everything?qInTitle={$sigla}}&excludeDomains={$domains}&pageSize=7&sortBy=publishedAt&language=pt&apiKey=22dc7327a8924716b7a82420e10ad0da");
-
     echo $data;
 }
 getNews($_POST['sigla']);
